e94b0d298f7620219867441689a3684e9b3347bb
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-06-26 01:13:38 +0530

    added style.css and adding obstacles

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
+
+// - - - - - - - - - - variables - - - - - - - - - - //
+
+// variables for obstacle
+minHeight = 20;
+maxHeight = 100;
+minWidth = 10;
+maxWidth = 20;
+minGap = 200;
+maxGap = 500;
+
+var myObstacles = [];
+
+
+
+// variable Game Area: Canvas
 var gameArea = {
-	canvas.document.createElement("canvas"),
+	canvas:document.createElement("canvas"),
+	
+	// To start the game
 	start: function(){
 		this.canvas.height = 500;
 		this.canvas.width = 1200;
@@ -7,6 +25,7 @@ var gameArea = {
 		this.context=this.canvas.getContext("2d");
 	},
 
+	// To update the game
 	updateGameArea: function(){
 
 
@@ -14,13 +33,34 @@ var gameArea = {
 
 	// "clearRect" clears a rectangle of the given size and coordinates
 	clear:function(){
-		gameare.context.clearRect(0, 0, this.canvas.width, this.canvas.width);
+		gameArea.context.clearRect(0, 0, this.canvas.height, this.canvas.width);
 
 	},
 
 	// to end the game
 	stop:function(){
-		
-	}
 
+	}
+}
+
+
+
+// - - - - - - - - - - Functions - - - - - - - - - - //
+
+// function to start the game
+function startGame(){
+	gameArea.start();
+}
+
+// function for obstacles
+function obstacle(){
+	this.height = Math.floor(minHeight + Math.random()*(maxHeight-minHeight+1));
+	this.width = Math.floor(minWidth + Math.random()*(maxWidth-minWidth+1));
+
+	// drawing the obstacle
+	this.x = 1200;
+	this.y = gameArea.canvas.height - this.height;
+	this.draw = function(){
+		gameArea.context.fillRect(this.x, this.y, this.width, this.height);
+	}
 }
